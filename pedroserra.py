fe3ac90279fcf46caa9f0d27604faa22d578e7c6
Pedro Serra <127122609+PedroMSerra@users.noreply.github.com>
2023-05-28 00:57:51 +0100

    Initial commit

---
+++ pedroserra.py
@@ -0,0 +1,18 @@
+def calculator(operation, num1, num2):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return 'Erro: divisão por zero'
+
+def check_parity(number):
+    if number % 2 == 0:
+        return 'par'
+    else:
+        return 'ímpar'
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+    name='pedroserra',
+    version='1.0.0',
+    author='Pedro Serra',
+    description='Módulo de operações aritméticas',
+    py_modules=['arithmetic'],
+)
